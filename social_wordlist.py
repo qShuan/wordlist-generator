56453f8d6f125a0c6b80e54da77336bf0039555e
qShuan <79919350+qShuan@users.noreply.github.com>
2022-10-01 21:06:12 +0200

    Update social_wordlist.py

--- social_wordlist.py
+++ social_wordlist.py
@@ -1,29 +1,42 @@
+from itertools import combinations, permutations
+
 
 author = "Shaun"
 
 val = input("How many words: ")
 keyword = [0]*int(val)
+special = [0]*6
+special[0] = "!"
+special[1] = "@"
+special[2] = "#"
+special[3] = "$"
+special[4] = "%"
+special[5] = "&"
+
+
+for i in range(0,len(special)):
+	print(special[i])
 
 value = int(val)
 
 for x in range(0, value):
 	keyword[x] = input("Value: ")
 
-
-
 #FUNCTIONS
 def first(f):
 	
 	i = 0
 	while(i<value - 1):
-	
+		
 		#NORMAL
+		#f.write(keyword[0]+'\n')
+		#f.write(keyword[0][::-1]+'\n')
 		f.write(keyword[0] + keyword[i+1]+ '\n')
 		f.write(keyword[0] + "_" + keyword[i+1]+ '\n')
 		
 		for k in range(1, len(keyword) + 1):
 			#Frist + other with underscore
-			f.write(keyword[0] + keyword[i+1] + "_" + keyword[value-k-i]+ '\n')
+			f.write(keyword[0] + keyword[i+1] + "_" + keyword[value-k-i] + '\n')
 			f.write(keyword[0] + "_" + keyword[i+1] + keyword[value-k-i]+ '\n')
 			#First + others
 			f.write(keyword[0] + keyword[i+1] + keyword[value-k-i]+ '\n')
@@ -48,6 +61,7 @@ def first(f):
 		
 		for k in range(1, len(keyword) + 1):
 			f.write(keyword[0][::-1] + "_" + keyword[i+1] + keyword[value-k-i]+ '\n')
+			f.write(keyword[0][::-1] + keyword[i+1] + "_" + keyword[value-k-i]+ '\n')
 		i+=1
 		
 		
@@ -56,6 +70,8 @@ def last(f):
 	while(k>0):
 	
 		#NORMAL
+		#f.write(keyword[value-1]+'\n')
+		#f.write(keyword[value-1][::-1]+'\n')
 		f.write(keyword[value-1] + keyword[k-1]+ '\n')
 		f.write(keyword[value-1] + "_" + keyword[k-1]+ '\n')
 		
@@ -72,6 +88,7 @@ def last(f):
 		
 		for l in range(0, len(keyword)):
 			f.write(keyword[value-1][::-1] + keyword[k-1] + "_" + keyword[0+l]+ '\n')
+			f.write(keyword[value-1][::-1] + "_" + keyword[k-1] + keyword[0+l]+ '\n')
 			
 		#LAST REVERSED
 		f.write(keyword[value-1] + keyword[k-1][::-1]+ '\n')
@@ -94,6 +111,8 @@ def iteratively(f):
 	i = 0
 	while(i < value-1):
 		#NORMAL
+		f.write(keyword[i]+'\n')
+		f.write(keyword[i][::-1]+'\n')
 		f.write(keyword[i] + keyword[i+1]+ '\n')
 		f.write(keyword[i] + "_" + keyword[i+1]+ '\n')
 			
@@ -111,6 +130,7 @@ def iteratively(f):
 		
 		for k in range(1, len(keyword) + 1):
 			f.write(keyword[i][::-1] + "_" + keyword[i+1] + keyword[value-k-i]+ '\n')
+			f.write(keyword[i][::-1] + keyword[i+1] + "_" + keyword[value-k-i]+ '\n')
 			
 		#LAST REVERSED
 		f.write(keyword[i] + keyword[i+1][::-1]+ '\n')
@@ -132,6 +152,8 @@ def iteratively(f):
 	k = value - 1
 	while(k > 0):
 		#NORMAL
+		f.write(keyword[k]+'\n')
+		f.write(keyword[k][::-1]+'\n')
 		f.write(keyword[k] + keyword[k-1]+ '\n')
 		f.write(keyword[k] + "_" + keyword[k-1]+ '\n')
 		
@@ -149,6 +171,7 @@ def iteratively(f):
 		
 		for l in range(0, len(keyword)):
 			f.write(keyword[k][::-1] + keyword[k-1] + "_" + keyword[0+l]+ '\n')
+			f.write(keyword[k][::-1] + "_" + keyword[k-1] + keyword[0+l]+ '\n')
 			
 		#LAST REVERSED
 		for l in range(0, len(keyword)):
@@ -170,7 +193,11 @@ def all_reverse(f):
 		f.write(keyword[0][::-1] + keyword[i+1][::-1] + '\n')
 		f.write(keyword[0][::-1] + "_" + keyword[i+1][::-1] + '\n')
 		
+		
+		
 		for k in range(1, len(keyword) + 1):
+			#First + rest
+			f.write(keyword[0][::-1] + keyword[i+1][::-1] + keyword[value-k-i][::-1]+ '\n')
 			#Underscore first
 			f.write(keyword[0][::-1] + "_" + keyword[i+1][::-1] + keyword[value-k-i][::-1]+ '\n')
 			f.write(keyword[0][::-1] + "_" + keyword[i+1][::-1] + keyword[value-k-i][::-1]+ '\n')
@@ -188,6 +215,8 @@ def all_reverse(f):
 		f.write(keyword[value-1][::-1] + "_" + keyword[k-1][::-1] + '\n')
 		
 		for l in range(0, len(keyword)):
+			#First + rest
+			f.write(keyword[value-1][::-1] + keyword[k-1][::-1] + keyword[0+l][::-1]+ '\n')
 			#Underscore first
 			f.write(keyword[value-1][::-1] + "_" + keyword[k-1][::-1] + keyword[0+l][::-1]+ '\n')
 			f.write(keyword[value-1][::-1] + "_" + keyword[k-1][::-1] + keyword[0+l][::-1]+ '\n')
@@ -208,4 +237,3 @@ with open('wordlist.txt', 'w') as f:
 	last(f)
 	iteratively(f)
 	all_reverse(f)
-
